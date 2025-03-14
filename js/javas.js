18fb8c0135e52198a7548be50881252b7c7770bc
Enes Çalış <bgenes145@gmail.com>
2025-03-14 21:00:59 +0300

    butona bastıkça renk değiştirecek bir javascript kodu ekledim

--- js/javas.js
+++ js/javas.js
@@ -1,1 +1,7 @@
 
+function changeColor() {
+    const colors = ['#46FF12', '#33FF57', '#3357FF', '#FFFF33', '#FF33FF'];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomIndex];
+}
+
